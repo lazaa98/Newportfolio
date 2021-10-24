702b3a96c9702bf492cb62accd11ac98d23585ae
lazaa98 <70294118+lazaa98@users.noreply.github.com>
2021-10-24 20:00:46 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -37,3 +37,21 @@ function topFunction() {
     document.body.scrollTop = 0;
     document.documentElement.scrollTop = 0;
 }
+
+
+const progressBar = document.querySelector('#progressBar');
+const section = document.querySelector('#sectionWrap');
+
+const animateProgressBar = () => {
+    let scrollDistance = -section.getBoundingClientRect().top;
+    let progressWidth = (scrollDistance / (section.getBoundingClientRect().height -document.documentElement.clientHeight)) * 100;
+    let value = Math.floor(progressWidth);
+    progressBar.style.width = value + "%"
+
+    if(value < 0){
+        progressBar.style.width = '0%'
+    }
+
+};
+
+window.addEventListener('scroll',animateProgressBar);
